8721d699ba417f5e525664706a680159ba1c3b62
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2025-01-14 06:18:22 +0600

    Update

---
+++ class-9/fl/Old/script.js
@@ -0,0 +1,18 @@
+/*
+// Select all cards
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  const heading = card.querySelector("h1");
+
+  // Add mouseover event
+  card.addEventListener("mouseover", () => {
+    heading.textContent = "Japan"; // Change text to Japan
+  });
+
+  // Add mouseout event
+  card.addEventListener("mouseout", () => {
+    heading.textContent = "Bangladesh"; // Change text back to Bangladesh
+  });
+});
+*/
